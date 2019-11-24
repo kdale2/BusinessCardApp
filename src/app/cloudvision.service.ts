4634ecc19f26a5a7b5cefc947835d7556335a1af
Kris Dale <kmdale2@gmail.com>
2019-11-24 17:32:27 -0600

    cloud vision api working

--- src/app/cloudvision.service.ts
+++ src/app/cloudvision.service.ts
@@ -5,6 +5,7 @@ import { environment } from 'src/environments/environment';
 import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 import { BusinessCardsComponent } from './business-cards/business-cards.component';
 import { catchError, map, tap, retry } from 'rxjs/operators';
+import { BusinessCardService } from './business-card.service';
 
 @Injectable({
   providedIn: 'root'
@@ -14,49 +15,64 @@ export class CloudvisionService {
   url: string;
   businessCard: BusinessCardComponent;
   value: any;
-  email: string; //for testing responses
+  firstName: string;
+  lastName: string;
+  company: string;
+  position: string;
+  address: string;
+  phone: string;
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient, private busCardService: BusinessCardService) { 
     this.url = `https://vision.googleapis.com/v1/images:annotate?key=${environment.cloudVisionKey}`;
   }
 
  postRequest(payload: any) {
+
     console.log("In image service post request");
     console.log("url: " + this.url);
 
     return this.http.post(this.url, payload).subscribe(
       (results: any) => { 
         console.log("Results from http request");
-        console.log(results);  
+        //console.log(results.description);
+        //console.log(results[1]);  
+        let text = results["responses"][0].fullTextAnnotation.text;
+        //this.txtano = "\n" + results.json().responses[0].fullTextAnnotation.text;
+        //console.log(text);
+        text = text.replace('\/', "");
+        //console.log(text);
+        this.createNewCard(text);
 
-        //const restString = [];
-        //console.log("Returned string: " + restString);
-        //console.log(response);
-        //this.getResponse();
-/*           value.forEach(
-          result => {
-            const temp = result.description;
-
-            console.log('response from cloud vision: ' + result.description);
-            console.log(result[0]);
-
-            // verify email
-            const email = (temp.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))(.*)$/) || [''])[0];
-            if (email !== '') {
-              this.email = email;
-              console.log("response putting in first name: " + this.email);
-              return;
-            }
-          }
-          );   */
-    }
-    );
+    });
   }  
 
-/* getResponse() {
-    console.log("in get response");
-    this.http.get('test').pipe(map(data => {console.log(data)}))
-      .subscribe(result => {console.log(result[0]);});
-  }  */
+  //need a function to parse out the text response
+    createNewCard(text: string): BusinessCardComponent {
+
+
+      if (text) {
+        let phoneMatches = text.match(/[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}/);
+        if (phoneMatches && phoneMatches.length > 0) {
+          this.phone = phoneMatches[0];
+          console.log("phone number: " + this.phone);
+        }
+
+        const line = text.split('\n');
+
+        this.firstName = line[0];
+        this.lastName = line[1];
+        this.company = line[2];
+        this.position = line[3]
+        this.address = line[4];
+        this.phone = line[5];
+
+      }
+
+      this.businessCard = new BusinessCardComponent(this.firstName, this.lastName, this.position, this.company, this.address, this.phone);
+      console.log(this.businessCard.address);
+      this.busCardService.createBusinessCard(this.businessCard);
+      return this.businessCard;
+    }
+
 }
 
